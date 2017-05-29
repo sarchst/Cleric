5637835139c1b99f5698b18a4c540e55114fc251
Gustav <yellingintothefan@gmail.com>
2017-05-29 14:01:20 -0700

    simplified things a little

--- main.py
+++ main.py
@@ -1,32 +1,18 @@
 import pygame
 import random
 
-class Circle:
-    def __init__(self, point):
-        self.point = point
-        self.size = 5
-        self.color = (0, 255, 0)
-        self.thickness = 1
-
-    def draw(self, screen):
-        pygame.draw.circle(screen, self.color, self.point, self.size, self.thickness)
-
-
 def main():
     xres = 640
     yres = 480
+    black = (0x00, 0x00, 0x00)
+    green = (0x00, 0xFF, 0x00)
     # Game init
     screen = pygame.display.set_mode((xres, yres))
     pygame.display.set_caption("Cleric")
-    screen.fill((0, 0, 0))
+    screen.fill(black)
     # Game loop
     done = False
     while not done:
-        # Game data
-        x = random.randint(0, xres)
-        y = random.randint(0, yres)
-        point = (x, y)
-        circle = Circle(point)
         # Key input
         for event in pygame.event.get():
             # User pushed window X
@@ -37,7 +23,10 @@ def main():
                 if event.key == pygame.K_F1:
                     done = True
         # Renderer
-        circle.draw(screen)
+        x = random.randint(0, xres)
+        y = random.randint(0, yres)
+        point = (x, y)
+        pygame.draw.circle(screen, green, point, 5, 1)
         pygame.display.flip()
         pygame.time.wait(15)
     # Done
