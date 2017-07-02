a7ae583866c4bd87a89f4b8a00b1b7b316b9ef58
Gustav <yellingintothefan@gmail.com>
2017-07-02 06:10:29 -0700

    animation + documentation: main.py -> cleric.py

--- cleric.py
+++ cleric.py
@@ -1,5 +1,4 @@
-import os
-import pygame
+import os, pygame
 
 class User:
     def __init__(self):
@@ -57,103 +56,172 @@ class User:
                 self.scroll_wheel -= 1
 
     def get_input(self):
-        # Wait here until any event occurs
-        event = pygame.event.wait()
+        """
+        Waits here until any mouse button (on the upstroke)
+        or the keyboard button event occurs (on the downstroke)
+        """
         # A full screen render will occur everytime the
         # mouse is moved, and that will kill the poor little CPU,
         # so wait for a key press or mouse press event
-        while event.type not in [pygame.MOUSEBUTTONUP, pygame.KEYUP]:
+        event = pygame.event.wait()
+        while event.type not in [pygame.MOUSEBUTTONUP, pygame.KEYDOWN]:
             event = pygame.event.wait()
         # Now service the mouse press event
         self.cursor_pixel = pygame.mouse.get_pos()
         if event.type == pygame.MOUSEBUTTONUP:
             self.serve_mouse(event)
         # ...Or the key press event
-        if event.type == pygame.KEYUP:
+        if event.type == pygame.KEYDOWN:
             self.serve_keyboard(event)
 
 class Catalog:
     def __init__(self):
+        # The current page number of the catalog
         self.page_number = 0
+        # All images loaded into a list
         self.pages = []
+        # The number of pages in the catalog
         self.page_count = 0
-        self.chapter_size = []
+        # The number of pages in each chapter
+        self.chapter_pages = []
+        # Available chapters
         self.chapters = [0, 1, 2, 3, 4, 5]
-        self.animations_per_tile = 2
+        # The number of pages needed to animate a tile
+        self.pages_per_animation = 2
 
     def load(self, root):
+        """
+        Loads the catalog from file. The catalog is divided into chapters.
+        The higher the chapter, the higher the importance of render.
+        For instance:
+        Chapter 0: Terrain
+        Chatper 1: Rocks / Leaves
+        Chapter 2: Characters
+        ...
+        Chapter 5: GUI
+        Rocks will always be rendered over terrain, characters always over rocks,
+        and the GUI always over everything.
+        """
         for chapter in self.chapters:
             parent = root + "/" + str(chapter)
             imgs = sorted(os.listdir(parent))
             for img in imgs:
                 page = pygame.image.load(parent + "/" + img)
                 self.pages.append(page)
-            self.chapter_size.append(len(imgs))
-            self.page_count = len(self.pages)
+            self.chapter_pages.append(len(imgs))
+        self.page_count = len(self.pages)
 
     def get_chapter(self):
+        """
+        Returns the chapter of the current catalog page
+        """
         for chapter in self.chapters:
-            if self.page_number < sum(self.chapter_size[:chapter + 1]):
+            if self.page_number < sum(self.chapter_pages[:chapter + 1]):
                 return chapter
         return None
 
     def bound(self, user):
-        self.page_number = self.animations_per_tile * user.scroll_wheel
+        """
+        Sets the catalog page to that of the mouse scroll index.
+        Forces the mouse scroll index back into place if it exceeds the number
+        of pages of the catalog, or if it less than 0
+        """
+        self.page_number = self.pages_per_animation * user.scroll_wheel
         # Upper bound
         if self.page_number >= self.page_count:
-            self.page_number = self.page_count - self.animations_per_tile
+            self.page_number = self.page_count - self.pages_per_animation
         # Lower bound
         if self.page_number < 0:
             self.page_number = 0
         # Force scroll wheel into place
-        user.scroll_wheel = self.page_number / self.animations_per_tile
+        user.scroll_wheel = self.page_number / self.pages_per_animation
 
 
 class Link:
+    """
+    Contains the relevant information needed to link a catalog
+    tile to a video layer. Chapter to video layer links are one to one,
+    that is:
+    Chapter 0 == Layer 0
+    Chapter 1 == Layer 1
+    Chapter 2 == Layer 2
+    ...
+    Chapter 5 == Layer 5 
+    """
     def __init__(self, user, catalog):
+        # The selected catalog pixel
         self.cat_pixel_selected = user.cat_pixel_selected
+        # The selected map pixel
         self.map_pixel_selected = user.map_pixel_selected
+        # The page number of the catalog
         self.page_number = catalog.page_number
+        # The chapter of the catalog
         self.chapter = catalog.get_chapter()
 
 class Video:
     def __init__(self, res):
         self.screen = pygame.display.set_mode(res)
+        # Number of frame renders since game start
+        self.renders = 0
+        # Rendering layers, one for each chapter, where each chapter
+        # is a dictionary with the format:
+        # (x, y) : Tile
+        # (x, y) is in grid format, not pixel format, such that one unique
+        # tile grid element can be used per entry
         self.layers = [ {}, {}, {}, {}, {}, {} ]
+        # Tile width and size - required for pixel to grid transformations
         self.tile_width = 16
         self.tile_size = (self.tile_width, self.tile_width)
-        # Margins
+        # Screen margins
         self.top_left = (0, 0)
-        # Colors
-        self.black = (0, 0, 0)
+        # Screen colors
+        self.black = (0x00, 0x00, 0x00)
 
     def to_tile(self, pixel):
+        """
+        Transforms a pixel tuple coordinate to a grid tile coordinate
+        eg: (70, 33) -> (4, 2)
+        """
         return tuple(int(i / self.tile_width) for i in pixel)
 
     def to_pixel(self, tile):
+        """
+        Transforms a grid tuple coordinate to a pixel tile coordinate
+        eg: (4, 2) -> (64, 32)
+        """
         return tuple(int(i * self.tile_width) for i in tile)
 
     def snap(self, pixel):
+        """
+        Snaps a pixel coordinate to the grid:
+        eg: (70, 33) -> (4, 2) -> (64, 32)
+        """
         tile = self.to_tile(pixel)
         return self.to_pixel(tile)
 
-    def create(self, link):
+    def place(self, link):
+        """
+        Places a tile link in a video layer
+        """
         map_tile = self.to_tile(link.map_pixel_selected)
         self.layers[link.chapter][map_tile] = link
 
     def save(self):
+        """
+        Saves the video layers to a pickle file
+        """
         pass
 
     def load(self):
+        """
+        Loads the video layers from a pickle file
+        """
         pass
 
-    def flip(self):
-        pygame.display.flip()
-
-    def wipe(self):
-        self.screen.fill(self.black)
-
     def blit_selector(self, user, catalog):
+        """
+        Buffers the mouse selector tile in the screen backbuffer
+        """
         # The GUI page is the very last page of the catalog
         gui_page = catalog.pages[catalog.page_count - 1]
         # The pixel of the selctor tile is at (10, 0) of the GUI page
@@ -164,37 +232,66 @@ class Video:
         self.screen.blit(gui_page, cursor_pixel, selector_pixel_rect)
 
     def blit_catalog(self, catalog):
+        """
+        Buffers a catalog page in the screen backbuffer
+        """
         page = catalog.pages[catalog.page_number]
         self.screen.blit(page, self.top_left)
 
     def blit_map(self, catalog):
+        """
+        Buffers the map in the screen backbuffer
+        """
+        animation = self.renders % 2
         for layer in self.layers:
             for tile in layer:
                 link = layer[tile]
-                page = catalog.pages[link.page_number]
+                page = catalog.pages[link.page_number + animation]
                 map_pixel_rect = (self.snap(link.map_pixel_selected), self.tile_size)
                 cat_pixel_rect = (self.snap(link.cat_pixel_selected), self.tile_size)
                 self.screen.blit(page, map_pixel_rect, cat_pixel_rect)
 
-def main():
+    def blit_clear(self):
+        """
+        Buffers blackness to the screen backbuffer
+        """
+        self.screen.fill(self.black)
+
+    def flip(self):
+        """
+        Transfers the screen backbuffer to the display
+        """
+        pygame.display.flip()
+        self.renders += 1
+
+def cleric():
+    """
+    Copyright (c) Sarchen Starke, Gustav Louw
+    """
+    # Game setup
     pygame.init()
     pygame.display.set_caption("Cleric")
     video = Video((640, 480))
     catalog = Catalog()
     catalog.load("dawnlike")
     user = User()
+    # Game loop
     while not user.is_done:
+        # Input
         user.get_input()
         catalog.bound(user)
-        video.wipe()
+        # Render
+        video.blit_clear()
         if user.map_pixel_selected and user.cat_pixel_selected:
-            video.create(Link(user, catalog))
+            video.place(Link(user, catalog))
         if user.is_catalogging:
             video.blit_catalog(catalog)
         else:
             video.blit_map(catalog)
         video.blit_selector(user, catalog)
         video.flip()
+    # Game done
     pygame.quit()
 
-main()
+# Rock and roll
+cleric()
