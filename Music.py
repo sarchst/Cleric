77b162176dcd802b89db20f681de686c0eccaa94
Gustav Louw <yellingintothefan@gmail.com>
2018-02-05 12:47:44 -0800

    shuffle

--- Music.py
+++ Music.py
@@ -6,21 +6,23 @@ class Music:
     """
     For everything music related
     """
-    
     def __init__(self):
-        soundTrack = os.listdir("music")
-        random.shuffle(soundTrack)
-        pg.mixer.init()
-        self._load("music/" + soundTrack.pop())
-        self._play()
+        self._queue_all_and_play()
 
-       
+    def _queue_all_and_play(self):
+        sound_track = os.listdir("music")
+        random.shuffle(sound_track)
+        pg.mixer.init()
+        self._load("music/" + sound_track.pop())
+        self._play()
+        for track in sound_track:
+            self._queue("music/" + track);
+
     def _load(self, track):
         pg.mixer.music.load(track)
 
     def _play(self):
         pg.mixer.music.play()
 
-    
     def _queue(self, track):
         pg.mixer.music.queue(track)
